5eb346191a98e63a4cf4cc5aec978ed45193ed6a
Cs <clswift1969@gmail.com>
2025-01-17 15:13:20 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket         = var.s3_bucket_name
+    key            = "terraform/terraform.tfstate"
+    region         = var.aws_region
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
