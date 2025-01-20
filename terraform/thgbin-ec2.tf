77609df6123ab7dcf0a939701557778aaba4d2fb
Cs <clswift1969@gmail.com>
2025-01-20 17:57:33 +0000

    Update thgbin-ec2.tf

--- terraform/thgbin-ec2.tf
+++ terraform/thgbin-ec2.tf
@@ -12,29 +12,17 @@ resource "aws_instance" "thgbin_instance" {
 
   user_data = <<-EOF
               #!/bin/bash
-              set -e  # Exit on any error
-              # Update and install dependencies
               sudo apt update -y
-              sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-
-              # Add Docker GPG key and repository
+              sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
               curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
               sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-              # Update and install Docker
               sudo apt update -y
-              sudo apt install -y docker-ce docker-ce-cli containerd.io
-
-              # Start and enable Docker
+              sudo apt install docker-ce docker-ce-cli containerd.io -y
               sudo systemctl start docker
               sudo systemctl enable docker
-
-              # Install Docker Compose
               curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-              sudo chmod +x /usr/local/bin/docker-compose
-
-              # Create necessary directories
-              sudo mkdir -p /opt/privatebin
+              chmod +x /usr/local/bin/docker-compose
+              mkdir -p /opt/privatebin
               EOF
 }
 
