9883e95391a512fdb6704a20b584c2dd8f2d4e41
CL1-96 <clswift1969@gmail.com>
2025-02-06 11:40:15 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,7 +41,7 @@ provider "cloudflare" {
 resource "cloudflare_record" "app" {
   zone_id = var.cloudflare_zone_id
   name    = "app"
-  value   = aws_instance.thgbin_instance.public_ip  # Uses EC2's public IP
+  content   = aws_instance.thgbin_instance.public_ip  # Uses EC2's public IP
   type    = "A"
   ttl     = 300
   proxied = true  # Enable Cloudflare Proxy
