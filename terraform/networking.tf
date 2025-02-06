cfc1925d87b89030de3d219fa8e2fbc313669887
CL1-96 <clswift1969@gmail.com>
2025-02-06 15:12:19 +0000

    add https ingress

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -30,6 +30,14 @@ resource "aws_security_group" "thgbin_sg" {
   }
 
   ingress {
+    description = "HTTPs access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "HTTP access"
     from_port   = 8080
     to_port     = 8080
